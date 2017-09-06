dbbc67d29549ced35ebd1b06b77e58a97d03b893
mohitpandey92 <mpandey@bu.edu>
2017-09-06 14:02:11 -0400

    ED working. Checking initial results

---
+++ zero_denom/python_code_ED/L_scaling/gauge_Lscaling.py
@@ -0,0 +1,101 @@
+from quspin.operators import hamiltonian # Hamiltonians and operators
+from quspin.basis import spin_basis_1d # Hilbert space spin basis
+import numpy as np # generic math functions
+import time
+
+t_start=time.time()
+
+def Ham_nonint(L):
+    basis = spin_basis_1d(L)
+    hz=(np.sqrt(5)+1)/4 #parameters used by Kim and Huse
+    hx=(np.sqrt(5)+5)/8
+    J=1.0
+    hz_arr = [[hz,i] for i in range(L)] 
+    hx_arr = [[hx,i] for i in range(L)] 
+    J_arr = [[1,i,(i+1)] for i in range(L-1)] # OBC [[J,i,(i+1)%L] for i in range(L)] # PBC
+    
+    # static and dynamic lists
+    static = [["xx",J_arr],["z",hz_arr], ["x",hx_arr] ]
+    dynamic =[]
+    H = hamiltonian(static,dynamic,basis=basis,dtype=np.complex_,check_symm=False,check_herm=False)
+    return H
+
+def Ham_int(L):
+    basis = spin_basis_1d(L)
+    hz=-5.0#(np.sqrt(5)+1)/4 #parameters used by Kim and Huse
+    J=-1.0
+    hz_arr = [[hz,i] for i in range(L)] # OBC
+    J_arr =  [[1,i,(i+1)] for i in range(L-1)] # OBC[[J,i,(i+1)%L] for i in range(L)] # PBC
+    
+    # static and dynamic lists
+    static = [["xx",J_arr],["z",hz_arr]]
+    dynamic =[]
+    H = hamiltonian(static,dynamic,basis=basis,dtype=np.complex_,check_symm=False,check_herm=False)
+    return H
+
+def del_lambda_Ham(L):
+    basis = spin_basis_1d(L)  
+    hx_lamb=-1.0
+    hx_lamb_arr=np.zeros(L)
+    hx_lamb_arr[0]=hx_lamb
+    hx_lamb_arr = [[hx_lamb_arr[i],i] for i in range(L)] # OBC
+    static_lamb = [["z",hx_lamb_arr]]
+    dynamic_lamb =[]
+    op_lamb=hamiltonian(static_lamb,dynamic_lamb,basis=basis,dtype=np.complex_,check_symm=False,check_herm=False)
+    return op_lamb
+
+def norm(A_lamb):    
+    return np.linalg.norm(A_lamb, 'fro')
+    
+def guage_potent_mu(Ham,L, mu):
+    E,V= Ham.eigh()
+    op_lamb= del_lambda_Ham(L)
+    wij = np.outer(E,np.ones(2**L))-np.outer(np.ones(2**L),E)
+    num_lamb = np.dot(V,np.dot(op_lamb.toarray(),np.conj(V)))
+    A_lamb = -1j*num_lamb*wij/(wij**2+ mu**2)
+    return A_lamb    
+    
+mu=np.sqrt(np.spacing(1))
+Ntot=9
+length=np.zeros(Ntot-2)
+norm_arr_int=np.zeros(Ntot-2)
+norm_arr_nonint=np.zeros(Ntot-2)
+
+for i in range(2,Ntot):
+    L=i
+    length[i-2]=i
+    H=Ham_int(L)
+    A_lamb=guage_potent_mu(H,L,mu)
+    norm_arr_int[i-2]=norm(A_lamb)
+    
+    
+    
+for i in range(2,Ntot):
+    L=i
+    length[i-2]=i
+    H=Ham_nonint(L)
+    A_lamb=guage_potent_mu(H,L,mu)
+    norm_arr_nonint[i-2]=norm(A_lamb)
+    
+t_end=time.time()
+
+t_code=(t_end-t_start)/60    
+
+
+f=open('Int_Lscaling.dat','w')
+f.write(" mu^2=%e, code time=%f (in min) \n"  %(mu**2,t_code))
+f.write('"mu" \t \t "||A||" \n')
+
+np.savetxt(f, np.transpose([length,norm_arr_int]) , fmt='%.12f', delimiter='\t')
+f.close()
+
+f=open('Non_int_Lscaling.dat','w')
+f.write(" mu^2=%e, code time=%f (in min) \n"  %(mu**2,t_code))
+f.write('"mu" \t \t "||A||" \n')
+
+
+np.savetxt(f, np.transpose([length,norm_arr_nonint]) , fmt='%.12f', delimiter='\t')
+f.close()
+
+
+
