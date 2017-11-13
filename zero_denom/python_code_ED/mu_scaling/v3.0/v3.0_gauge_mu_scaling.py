305a071e5b817ea3e5eae36942b108508bc40f58
mohitpandey92 <mpandey@bu.edu>
2017-11-13 17:27:14 -0500

    dk

--- zero_denom/python_code_ED/mu_scaling/v3.0/v3.0_gauge_mu_scaling.py
+++ zero_denom/python_code_ED/mu_scaling/v3.0/v3.0_gauge_mu_scaling.py
@@ -5,8 +5,6 @@ import time
 import sys
 
 L = int(sys.argv[1])
-
-
 t_start=time.time()
 
 def Ham_nonint(L):
@@ -21,8 +19,9 @@ def Ham_nonint(L):
     # static and dynamic lists
     static = [["zz",J_arr],["z",hz_arr], ["x",hx_arr] ]
     dynamic =[]
-    H = hamiltonian(static,dynamic,basis=basis,dtype=np.complex_,check_symm=False,check_herm=False)
+    H = hamiltonian(static,dynamic,basis=basis,dtype=np.float_,check_symm=False,check_herm=False)
     return H
+
 
 def Ham_int_antiferro(L,hz):
     basis = spin_basis_1d(L)
@@ -32,8 +31,21 @@ def Ham_int_antiferro(L,hz):
     # static and dynamic lists
     static = [["zz",J_arr],["x",hz_arr]]
     dynamic =[]
-    H = hamiltonian(static,dynamic,basis=basis,dtype=np.complex_,check_symm=False,check_herm=False)
+    H = hamiltonian(static,dynamic,basis=basis,dtype=np.float_,check_symm=False,check_herm=False)
     return H
+
+
+def output_gauge_potent(Ham,L):
+    E,V= Ham.eigh()
+    op_lamb_arr = del_lambda_Ham(L).toarray()
+    x=np.dot(op_lamb_arr, V)
+    num_lamb_mat =np.dot(V.T,x) #matrix multiplication
+    wij = np.outer(E,np.ones(2**L))-np.outer(np.ones(2**L),E)
+    return wij, num_lamb_mat
+  
+def gauge_potent_mu(wij,num_lamb_mat,mu):
+    A_lamb = -1j*np.multiply(wij,num_lamb_mat)/(wij**2+ mu**2)#element-wise multiplication
+    return A_lamb
 
 
 def del_lambda_Ham(L):
@@ -42,29 +54,15 @@ def del_lambda_Ham(L):
     hx_lamb_arr = [[hx_lamb,i] for i in range(L)] 
     static_lamb = [["x",hx_lamb_arr]]
     dynamic_lamb =[]
-    op_lamb=hamiltonian(static_lamb,dynamic_lamb,basis=basis,dtype=np.complex_,check_symm=False,check_herm=False)
+    op_lamb=hamiltonian(static_lamb,dynamic_lamb,basis=basis,dtype=np.float_,check_symm=False,check_herm=False)
     return op_lamb
 
 def norm(A_lamb):    
     return np.linalg.norm(A_lamb, 'fro')
 
-def output_gauge_potent(Ham,L):
-	E,V= Ham.eigh()
-	V_mat=np.matrix(V)
-    	V_mat_H=V_mat.H
-    	op_lamb_mat = np.matrix(del_lambda_Ham(L).toarray())
-    	num_lamb_mat =  (V_mat_H)*(op_lamb_mat*V_mat) #matrix multiplication
-    	wij = np.outer(E,np.ones(2**L))-np.outer(np.ones(2**L),E)
-	return wij, num_lamb_mat
-  
-def gauge_potent_mu(wij,num_lamb_mat,mu):
-    A_lamb = -1j*np.multiply(wij,num_lamb_mat)/(wij**2+ mu**2)#element-wise multiplication
-    return A_lamb
-
 ###parameters
-muTot=40
-mu_arr=np.logspace(-6,1.0,muTot)
-
+muTot=200
+mu_arr=np.logspace(-2,1.0,muTot)
 norm_arr_nonint=np.zeros(muTot)
 norm_arr_int=np.zeros(muTot)
 hz=5.00#for int Ham
@@ -112,22 +110,20 @@ t_int_code=(t_end_int-t_start_int)/60
 
 
 
-#f=open('Test_v2.0_L%s_nonint_mu_scaling.dat' %L,'w')
-#f.write(" L=%d, code time=%f (in min) \n"  %(L,t_nonint_code))
-#f.write("wij_min=%.20e, wij_max=%.20e \n"  %(wij_min_nonint,wij_max_nonint))
-#f.write('"mu" \t \t "||A||^2" \n')
+f=open('v3.0_L%s_nonint_mu_scaling.dat' %L,'w')
+f.write(" L=%d, code time=%f (in min) \n"  %(L,t_nonint_code))
+f.write("wij_min=%.20e, wij_max=%.20e \n"  %(wij_min_nonint,wij_max_nonint))
+f.write('"mu" \t \t "||A||^2" \n')
 
 
-#np.savetxt(f, np.transpose([mu_arr,norm_arr_nonint**2]) , fmt='%.20e', delimiter='\t')
-#f.close()
+np.savetxt(f, np.transpose([mu_arr,norm_arr_nonint**2]) , fmt='%.20e', delimiter='\t')
+f.close()
 
 
-#f=open('Test_v2.0_L%s_int_mu_scaling.dat' %L, 'w')
-#f.write("L=%d, code time=%f (in min) \n"  %(L,t_int_code))
-#f.write("wij_min=%.20e, wij_max=%.20e \n"  %(wij_min_int,wij_max_int))
-#f.write('"mu" \t \t "||A||^2" \n')
+f=open('v3.0_L%s_int_mu_scaling.dat' %L, 'w')
+f.write("L=%d, code time=%f (in min) \n"  %(L,t_int_code))
+f.write("wij_min=%.20e, wij_max=%.20e \n"  %(wij_min_int,wij_max_int))
+f.write('"mu" \t \t "||A||^2" \n')
 
-#np.savetxt(f, np.transpose([ mu_arr,norm_arr_int**2]) , fmt='%.20e', delimiter='\t')
-#f.close()
-
-
+np.savetxt(f, np.transpose([ mu_arr,norm_arr_int**2]) , fmt='%.20e', delimiter='\t')
+f.close()
